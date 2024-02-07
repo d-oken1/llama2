b499e862ea7044ad4b21559a61ec37104df01b38
d-oken1 <138194925+d-oken1@users.noreply.github.com>
2024-02-07 16:53:33 -0500

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ with st.sidebar:
     else:
         replicate_api = st.text_input('Enter Replicate API token:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-            st.warning('Please enter your credentials!', icon='⚠️')
+            st.warning('Please enter your credentials honey!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
